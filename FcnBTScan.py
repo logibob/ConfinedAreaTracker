130df4b9e9ac4841611f24b05198a96ef315fe29
logibob <arndt.seb@gmail.com>
2021-10-07 00:25:53 +0200

    start implementing objects for BT devices; class created

--- FcnBTScan.py
+++ FcnBTScan.py
@@ -4,12 +4,6 @@ class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
 
-    # def handleDiscovery(self, dev, isNewDev, isNewData):
-    #     if isNewDev:
-    #         print ("Discovered device", dev.addr)
-    #     elif isNewData:
-    #         print ("Received new data from", dev.addr)
-
 def BTScan():
 
     # scan and register in object list "devices"
